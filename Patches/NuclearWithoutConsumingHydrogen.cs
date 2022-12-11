915cb898357e9b3df409ebe805afbbf6f7c271bf
Gregory Brutsche <gregory@mail.brutsches.local>
2022-12-11 13:38:33 -0500

    Nuke power without consuming hydrogen.

---
+++ Patches/NuclearWithoutConsumingHydrogen.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using BulwarkStudios.Stanford.Torus.Buildings;
+using HarmonyLib;
+
+namespace GMod.Patches;
+
+// ReSharper disable RedundantAssignment, UnusedMember.Global
+[HarmonyPatch]
+public class NuclearWithoutConsumingHydrogen {
+    [HarmonyTargetMethod]
+    public static MethodBase TargetMethod() {
+        return typeof(CommandBuildingNuclearPowerPlant).GetMethod(nameof(CommandBuildingNuclearPowerPlant.OnTick), BindingFlags.Public | BindingFlags.Instance | BindingFlags.FlattenHierarchy);
+    }
+
+    [HarmonyPrefix]
+    public static bool Prefix(ref CommandBuildingNuclearPowerPlant __instance, ref float deltaTime) {
+        deltaTime = 0f;
+        return true;
+    }
+}
