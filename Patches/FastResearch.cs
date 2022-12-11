122473b439137967e60d9fc112ffca5a05afde3c
Gregory Brutsche <gregory@mail.brutsches.local>
2022-12-11 14:39:26 -0500

    Very fast research speed.

---
+++ Patches/FastResearch.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using BulwarkStudios.Stanford.Common.TechTree;
+using HarmonyLib;
+
+namespace GMod.Patches;
+
+// ReSharper disable RedundantAssignment, UnusedMember.Global
+[HarmonyPatch]
+public class FastResearch {
+    [HarmonyTargetMethod]
+    public static MethodBase TargetMethod() {
+        return typeof(CommandResearchTechnology).GetMethod(nameof(CommandResearchTechnology.BulwarkStudios_Stanford_Core_Commands_ICommandCustomTickable_OnCustomTick), BindingFlags.Public | BindingFlags.Instance | BindingFlags.FlattenHierarchy);
+    }
+
+    [HarmonyPrefix]
+    public static bool Prefix(ref float deltaTime) {
+        deltaTime = 10f;
+        return true;
+    }
+}
