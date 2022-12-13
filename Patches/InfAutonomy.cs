ef905b93d65b9229d02f12d61366cf4041d01a3a
Gregory Brutsche <gregory@mail.brutsches.local>
2022-12-13 01:03:19 -0500

    Add infinite autonomy.

---
+++ Patches/InfAutonomy.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using BulwarkStudios.Stanford.SolarSystem.SpaceVehicles.Commands;
+using HarmonyLib;
+
+namespace GMod.Patches;
+
+// ReSharper disable RedundantAssignment, UnusedMember.Global
+[HarmonyPatch]
+public class InfAutonomy {
+    [HarmonyTargetMethod]
+    public static MethodBase TargetMethod() {
+        return typeof(CommandAutonomousSpaceVehicle).GetMethod(nameof(CommandAutonomousSpaceVehicle.BulwarkStudios_Stanford_Core_Commands_ICommandCustomTickable_OnCustomTick), BindingFlags.Public | BindingFlags.Instance | BindingFlags.FlattenHierarchy);
+    }
+
+    [HarmonyPrefix]
+    public static bool Prefix(ref CommandAutonomousSpaceVehicle __instance) {
+        __instance.spaceVehicle.State.AutonomyLeft = 10f * 60f; // 60 == 1 cycle.
+        return true;
+    }
+}
